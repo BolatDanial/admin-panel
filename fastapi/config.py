94b4df9dd87ba2b379efac08394791f295972218
BolatDanial <230412@astanait.edu.kz>
2025-06-02 10:47:40 +0500

    Fixed

--- fastapi/config.py
+++ fastapi/config.py
@@ -4,11 +4,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 admin_db_connect = os.getenv("DB_CONNECT")
-print(admin_db_connect)
 
 JWT_SECRET = os.getenv("JWT_SECRET")
 JWT_ALGORITHM = os.getenv("JWT_ALGORITHM")
-TOKEN_EXPIRE_MINUTES = os.getenv("TOKEN_EXPIRE_MINUTES")
+TOKEN_EXPIRE_MINUTES = int(os.getenv("TOKEN_EXPIRE_MINUTES"))
 
 UPLOAD_DIR = "./uploads"
 os.makedirs(UPLOAD_DIR, exist_ok=True)
